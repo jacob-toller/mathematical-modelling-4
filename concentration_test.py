32df4fe1d1625e8a88fb69e0b1d911c9e972da17
JacobT <superlolcopter2@gmail.com>
2023-04-27 14:29:06 +0100

    initial commit of concentration test

---
+++ concentration_test.py
@@ -0,0 +1,27 @@
+import numpy as np
+np.set_printoptions(precision=2)
+
+grid_size = (10,10)
+a = 1
+b = 1
+
+conc_map = np.full(grid_size,0,dtype=np.float64)
+
+marker_tracker = [
+    (1,2),
+    (5,5)
+] # markers denoted in (x,y) format
+
+for i_y,i_x in np.ndindex(grid_size):
+    # this iterates over every single cell in the grid
+    print(i_y,i_x)
+    conc = 0
+    for marker in marker_tracker:
+        # this iterates over the markers
+        dist = ((marker[1] - i_y)**2 + (marker[0] - i_x)**2)**0.5 + 1
+        
+        conc += a/(dist**(b))
+
+    conc_map[i_y,i_x] = conc
+
+print(conc_map)
