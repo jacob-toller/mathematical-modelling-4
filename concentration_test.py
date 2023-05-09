d03d98fb51a3dda889514f2bfefc6db86034fd98
JacobT <superlolcopter2@gmail.com>
2023-05-09 17:49:20 +0100

    added save feature, turn around at objective flag for walkers

--- concentration_test.py
+++ concentration_test.py
@@ -1,20 +1,26 @@
 import numpy as np
 np.set_printoptions(precision=2)
 
-grid_size = (20,20)
+def latexprint(ndarray,prec = 2):
+    string = "\hline"
+    for row in ndarray:
+        string += "\n    " + " & ".join("{:.2f}".format(x) for x in row)
+        string += " \\\\ \n\hline"
+
+    print(string)
+
+grid_size = (6,6)
 a = 1
-b = 1
+b = 2
 
 conc_map = np.full(grid_size,0,dtype=np.float64)
 
 marker_tracker = [
-    (1,2),
-    (5,5)
+    (1,1),(4,5)
 ] # markers denoted in (x,y) format
 
 for i_y,i_x in np.ndindex(grid_size):
     # this iterates over every single cell in the grid
-    print(i_y,i_x)
     conc = 0
     for marker in marker_tracker:
         # this iterates over the markers
@@ -25,3 +31,5 @@ for i_y,i_x in np.ndindex(grid_size):
     conc_map[i_y,i_x] = conc
 
 print(conc_map)
+latexprint(conc_map)
+
